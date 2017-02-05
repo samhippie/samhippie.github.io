839faca7dc4d1d7d9ca5d3415a626df6007b8f7f
Sam Higginbotham <samhggnbthm@gmail.com>
2017-02-04 21:11:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,20 +8,24 @@ function push()
 	console.log("posting!");
 	var name = $("input")[0].value;
 	var icon = $("input")[1].value;
-	var text = $("input")[2].value.toLowerCase();
-	var x = $.ajax(
-			{
-				url: "https://discordapp.com/api/webhooks/277159899174010880/TMlj-Rm8PCoJDiVPzIvYv4pW-XXrz2XAJCxSJBZ1CVD0pXjNQNlnJRhHlcrMP9GY1Uxv",
-				data: JSON.stringify(
-					{"content": text,
-					 "tts": "true",
-					 "avatar_url": icon,
-					 "username": name
-					}),
-				dataType: "json",
-				contentType: "application/json",
-				method: "POST",
-				error: function(a, b, c) {console.log("oh no ", b);}
-			});
-	console.log(x);
+	var text = $("input")[2].value.toLowerCase().split(" ");
+	for(var i = 0; i < text.length; i++)
+	{
+		var x = $.ajax(
+				{
+					url: "https://discordapp.com/api/webhooks/277159899174010880/TMlj-Rm8PCoJDiVPzIvYv4pW-XXrz2XAJCxSJBZ1CVD0pXjNQNlnJRhHlcrMP9GY1Uxv",
+					data: JSON.stringify(
+						{"content": text[i],
+						 "tts": "true",
+						 "avatar_url": icon,
+						 "username": name
+						}),
+					dataType: "json",
+					contentType: "application/json",
+					method: "POST",
+					error: function(a, b, c) {console.log("oh no ", b);}
+				});
+
+		console.log(x);
+	}
 }
