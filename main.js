1746bcd07113faaea3063904efbfacf1ef358ee5
Sam Higginbotham <samhggnbthm@gmail.com>
2017-02-04 21:22:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,22 +13,20 @@ function push()
 
 
 
-		var x = $.ajax(
-				{
-					url: "https://discordapp.com/api/webhooks/277159899174010880/TMlj-Rm8PCoJDiVPzIvYv4pW-XXrz2XAJCxSJBZ1CVD0pXjNQNlnJRhHlcrMP9GY1Uxv",
-					data: JSON.stringify(
-						{"content": text,
-						 "tts": "true",
-						 "avatar_url": icon,
-						 "username": name
-						}),
-					dataType: "json",
-					contentType: "application/json",
-					method: "POST",
-					error: function(a, b, c) {console.log("oh no ", b);}
-				});
+	var x = $.ajax(
+			{
+				url: "https://discordapp.com/api/webhooks/277159899174010880/TMlj-Rm8PCoJDiVPzIvYv4pW-XXrz2XAJCxSJBZ1CVD0pXjNQNlnJRhHlcrMP9GY1Uxv",
+				data: JSON.stringify(
+					{"content": text,
+					 "tts": "true",
+					 "avatar_url": icon,
+					 "username": name
+					}),
+				dataType: "json",
+				contentType: "application/json",
+				method: "POST",
+				error: function(a, b, c) {console.log("oh no ", b);}
+			});
 
-		console.log(x);
-
-	}
+	console.log(x);
 }
