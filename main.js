acea740ed9503d724eac69302ed2e3dab59fc746
Sam Higginbotham <samhggnbthm@gmail.com>
2017-02-04 19:29:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,18 @@ window.onload = function()
 function push()
 {
 	console.log("posting!");
+	var icon = $("icon")[0].value;
+	var name = $("name")[0].value;
 	var x = $.ajax(
 			{
 				url: "https://discordapp.com/api/webhooks/277159899174010880/TMlj-Rm8PCoJDiVPzIvYv4pW-XXrz2XAJCxSJBZ1CVD0pXjNQNlnJRhHlcrMP9GY1Uxv",
 				//url: "https://posttestserver.com/post.php",
-				data: JSON.stringify({"content": $("input")[0].value, "tts": "true"}),
+				data: JSON.stringify(
+					{"content": $("input")[0].value,
+					 "tts": "true",
+					 "avatar_url": icon,
+					 "username": name
+					}),
 				dataType: "json",
 				contentType: "application/json",
 				method: "POST",
